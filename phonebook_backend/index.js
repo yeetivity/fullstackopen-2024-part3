76e2856a6c5aafbd07dea4aa7998e875854eae19
yeetivity <jj.ve.student@gmail.com>
2024-01-03 22:04:25 +0100

    part3; exercise 15-18 Added a testing file for deleting and updating persons. Updated the index.js so that the info, delete, and update route work and error handling is in middleware

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -6,6 +6,15 @@ const Person = require('./models/person')
 
 const app = express()
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
 // Create a custom morgan token
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
@@ -13,7 +22,6 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(cors())
 // Make sure to fetch the static page
 app.use(express.static('dist'))
-
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -40,10 +48,16 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).send('Person does not exist')
+        }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -71,13 +85,36 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-// Delete routes (DOESN'T WORK WITH MONGO YET)
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+// Delete routes
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+// Update route
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 // Running the application
 const PORT = process.env.PORT
