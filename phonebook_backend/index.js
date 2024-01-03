f8b256c9b5f94b7b1c5ae06f46d6ff30a48a8945
yeetivity <jj.ve.student@gmail.com>
2024-01-03 15:25:51 +0100

    part3; exercise 1-8 Created a backend repository for the phonebook

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+// Create a custom morgan token
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
